40a9af9976edbf57c6cc7d5019fbfb23d4de5845
Tommieboy99 <tomkoek99@hotmail.com>
2025-06-23 23:03:55 +0200

    Added an array for the books, added a construction function, added a function to create a new book and push this new book to the array

--- script.js
+++ script.js
@@ -1,41 +1,17 @@
-const myLibrary = [];
+const myLibrary = []
 
-function Book(title, author, pages, isRead) {
-    this.id = crypto.randomUUID();
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.isRead = isRead;
+function Book(newBook) {
+  this.id = crypto.randomUUID();
+  this.title = newBook.title,
+  this.author = newBook.author,
+  this.pages = newBook.pages,
+  this.status = newBook.status
 }
 
-function addBookToLibrary(title, author, pages, isRead) {
-    const newBook = new Book(title, author, pages, isRead);
-    myLibrary.push(newBook)
+function addBookToLibrary(title, author, pages, status) {
+  const newBook = new Book({title, author, pages, status});
+  myLibrary.push(newBook);
 }
 
-function displayBooks() {
-    const bookList = document.getElementById("book-list");
-    bookList.innerHTML = "";
-
-    myLibrary.forEach((book) => {
-        const bookDiv = document.createElement("div");
-        bookDiv.style.border = "0.5rem solid black";
-        bookDiv.style.padding = "1rem";
-        bookDiv.style.margin = "1rem";
-
-        bookDiv.innerHTML = `
-        <h3>${book.title}</h3>
-        <p>Author: ${book.author}</p>
-        <p>Pages: ${book.pages}</p>
-        <p>Read: ${book.isRead ? "Yes" : "No"}</p>
-        `;
-
-        bookList.appendChild(bookDiv);
-    });
-}
-
-addBookToLibrary("Pap, vertel eens...", "Elma van Vliet", 208, false);
-addBookToLibrary("De laatste ochtend", "Samuel Bjork", 400, false);
-addBookToLibrary("Vrees niet", "Stephen King", 496, false);
-
-displayBooks();
+console.log(addBookToLibrary("Catch me", "Tom Nibbet", 200, "not read"));
+console.log(addBookToLibrary("Not for you", "Rip", 100, "read"));
