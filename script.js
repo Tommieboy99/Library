a1dd9a565a742d0f108810b2a0dc349c92a5c87b
Tommieboy99 <tomkoek99@hotmail.com>
2025-06-24 16:10:46 +0200

    Added function bookForm, save the input values the user gave and pass the values in to addBookToLibrary() and display in the table

--- script.js
+++ script.js
@@ -2,10 +2,10 @@ const myLibrary = []
 
 function Book(newBook) {
   this.id = crypto.randomUUID();
-  this.title = newBook.title,
-  this.author = newBook.author,
-  this.pages = newBook.pages,
-  this.status = newBook.status
+  this.title = newBook.title;
+  this.author = newBook.author;
+  this.pages = newBook.pages;
+  this.status = newBook.status;
 }
 
 function addBookToLibrary(title, author, pages, status) {
@@ -13,30 +13,59 @@ function addBookToLibrary(title, author, pages, status) {
   myLibrary.push(newBook);
 }
 
-myLibrary.forEach(book => {
-  const tableBody = document.querySelector(".table-body");
-  const row = document.createElement("tr");
+function displayLibrary() {
+  myLibrary.forEach(book => {
+    const tableBody = document.querySelector(".table-body");
+    const row = document.createElement("tr");
   
-  const idCell = document.createElement("td");
-  idCell.textContent = book.id;
+    const idCell = document.createElement("td");
+    idCell.textContent = book.id;
 
-  const titleCell = document.createElement("td");
-  titleCell.textContent = book.title;
+    const titleCell = document.createElement("td");
+    titleCell.textContent = book.title;
 
-  const authorCell = document.createElement("td");
-  authorCell.textContent = book.author;
+    const authorCell = document.createElement("td");
+    authorCell.textContent = book.author;
 
-  const pagesCell = document.createElement("td");
-  pagesCell.textContent = book.pages;
+    const pagesCell = document.createElement("td");
+    pagesCell.textContent = book.pages;
 
-  const statusCell = document.createElement("td");
-  statusCell.textContent = book.status;
+    const statusCell = document.createElement("td");
+    statusCell.textContent = book.status;
 
-  row.appendChild(idCell);
-  row.appendChild(titleCell);
-  row.appendChild(authorCell);
-  row.appendChild(pagesCell);
-  row.appendChild(statusCell);
+    row.appendChild(idCell);
+    row.appendChild(titleCell);
+    row.appendChild(authorCell);
+    row.appendChild(pagesCell);
+    row.appendChild(statusCell);
 
-  tableBody.appendChild(row);
-});
+    tableBody.appendChild(row);
+})};
+
+const newBookBtn = document.querySelector(".btn-newBook");
+const dialog = document.querySelector("dialog");
+const closeDialogBtn = document.querySelector(".close-btn");
+
+newBookBtn.addEventListener("click", () => {
+  dialog.showModal();
+})
+
+closeDialogBtn.addEventListener("click", () => {
+  dialog.close();
+})
+
+const bookForm = document.querySelector("#bookForm");
+
+bookForm.addEventListener("submit", function(event) {
+  event.preventDefault();
+
+  const titleValue = document.querySelector('#title').value;
+  const authorValue = document.querySelector('#author').value;
+  const pagesValue = document.querySelector('#pages').value;
+  const statusValue = document.querySelector('#status').value;
+
+  addBookToLibrary(titleValue, authorValue, pagesValue, statusValue);
+  displayLibrary();
+
+  dialog.close();
+})
